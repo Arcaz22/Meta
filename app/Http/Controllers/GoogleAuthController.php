a85fd8d9583f0737eb19e26ee9e13f5b87aacbfc
Arcaz22 <chandraarcychan@gmail.com>
2022-12-03 13:25:46 +0700

    Auth Login & Registrasi menggunakan Breeze dan Socialize

---
+++ app/Http/Controllers/GoogleAuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleAuthController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callbackGoogle()
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+            $findUser = User::where('google_id', $user->getId())->first();
+
+            if($findUser)
+            {
+                Auth::login($findUser);
+                return redirect()->intended('dashboard');
+            } else {
+                $newUser = User::create([
+                    'name' => $user->getName(),
+                    'email' => $user->getEmail(),
+                    'google_id' => $user->getId(),
+                    'password' => bcrypt('12345678')
+                ]);
+                Auth::login($newUser);
+                return redirect()->intended('dashboard');
+            }
+        } catch (\Throwable $th) {
+            dd('Something went wrong! ' . $th->getMessage());
+        }
+    }
+}
