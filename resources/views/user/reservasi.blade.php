82a261cdfe92bbd2dd21d05a4a109ecb036a430f
akbarmauu <76439418+akbarmauu@users.noreply.github.com>
2022-12-27 22:06:57 +0700

    Fix CRUD

--- resources/views/user/reservasi.blade.php
+++ resources/views/user/reservasi.blade.php
@@ -116,7 +116,7 @@
                 </div>
               </div>
               <div>
-                <button type="submit" class="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Pesan</button>
+                <button type="submit" class="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" >Pesan</button>
               </div>
             </form>
            </div>
