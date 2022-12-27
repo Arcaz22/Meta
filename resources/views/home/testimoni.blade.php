d026226d0efbc6fba2936562d48d4ff9f9aafdb7
MTegarRosmali <02tegarrosmali@gmail.com>
2022-12-27 22:48:22 +0700

    fix

--- resources/views/home/testimoni.blade.php
+++ resources/views/home/testimoni.blade.php
@@ -11,9 +11,7 @@
     <section class="mb-20 text-gray-700">
       <div class="text-center md:max-w-xl lg:max-w-3xl mx-auto">
         <p class="pt-10  mb-6 pb-2 md:mb-12 md:pb-0">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam
-          iure provident voluptate esse quasi, veritatis totam voluptas nostrum quisquam eum
-          porro a pariatur veniam.
+          TestTing
         </p>
       </div>
     
